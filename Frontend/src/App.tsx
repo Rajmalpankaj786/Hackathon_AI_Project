6fc613974592a130aedbe0e38014e23ad92181a3
PatilSandhya <begadesandhya@gmail.com>
2023-11-11 22:25:21 +0530

    home

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import './App.css';
+import Navbar from './pages/Navbar';
+import HomePage from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-6xl text-red-500 bg-black'>Hackathon Project</h1>
+      <Navbar />
+      <HomePage />
     </div>
   );
 }
