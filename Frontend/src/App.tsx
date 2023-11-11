2131d7196936fb6378c65080c29fe4f44e470312
vk10082000 <vamshikrishna99089@gmail.com>
2023-11-11 13:45:46 +0530

    Frontend: Basic  Setup done

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-6xl text-red-500 bg-black'>Hackathon Project</h1>
+    </div>
+  );
+}
+
+export default App;
